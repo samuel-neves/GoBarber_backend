a954adfd086019cde497242866610661762e48f9
Samuel212584 <samuel212584@gmail.com>
2020-08-24 12:31:28 -0300

    Creating appointments changes

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -19,11 +19,13 @@ describe('ListProviderDayAvailability', () => {
   it('should be able to list providers day availability', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      user_id: 'user',
       date: new Date(2020, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      user_id: 'user',
       date: new Date(2020, 4, 20, 15, 0, 0),
     });
 
