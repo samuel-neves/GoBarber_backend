aa045de63c992d1cd7d4b34b4eaa73c730336f60
Samuel212584 <samuel212584@gmail.com>
2020-09-01 10:05:36 -0300

    Storage provider rename

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -2,7 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
-import IStorageProvider from '@shared/container/providers/DiskStorageProvider/models/IStorageProvider';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 import User from '../infra/typeorm/entities/User';
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,3 +1,3 @@
-import './DiskStorageProvider';
+import './StorageProvider';
 import './MailTemplateProvider';
 import './MailProvider';
