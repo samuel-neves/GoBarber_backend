bcf612967403500d09bf4a12952d01cdcd7e5f2f
Samuel212584 <samuel212584@gmail.com>
2020-09-05 10:44:36 -0300

    Class serialization in appointment and users

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointments';
 
 interface IRequest {
@@ -44,7 +45,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
