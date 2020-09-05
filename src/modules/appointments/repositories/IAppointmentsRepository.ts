735ca4af792f3682efccfe1b99abf747746334f3
Samuel212584 <samuel212584@gmail.com>
2020-09-05 10:15:10 -0300

    Provider_id in findByDate function

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -5,7 +5,7 @@ import IFindAllInDayDTO from '../dtos/IFindAllInDayDTO';
 
 export default interface IAppointmentRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  findByDate(date: Date, provider_id: string): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(data: IFindAllInMonthDTO): Promise<Appointment[]>;
   findAllInDayFromProvider(data: IFindAllInDayDTO): Promise<Appointment[]>;
 }
